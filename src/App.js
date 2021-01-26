cd93a313b636f929c97e1ed030b95f851f826c79
Nicolas <nicobsas+webdev@gmail.com>
2021-01-27 01:03:44 +0900

    start on login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./Chat";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState("xkime");
+
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome back home</h1>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome back home</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   );
--- src/Message.js
+++ src/Message.js
@@ -6,8 +6,13 @@ function Message({ message, user, timestamp, userImage }) {
     <div className="message">
       <img src={userImage} alt={user} />
       <div className="message__info">
-        <h4>{user}</h4>
-        <p>{message} timestamp</p>
+        <h4>
+          {user}{" "}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
+        </h4>
+        <p>{message}</p>
       </div>
     </div>
   );
