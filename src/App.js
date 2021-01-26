e5e41c4d2e652588f485df12dc496a52ab46e09f
Nicolas <nicobsas+webdev@gmail.com>
2021-01-27 01:52:07 +0900

    add state management hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./Chat";
 import Login from "./Login";
+import { useStateValue } from "./StateProvider";
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
 
   return (
     <div className="app">
